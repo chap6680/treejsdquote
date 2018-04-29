362eb389f0cff0cbb08951689df9a7409cfcf1ed
david chapman <chap6680@gmail.com>
2018-04-29 14:55:45 -0600

    last brainstorming...will begin to clean up after this

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const uiQuoteBox = document.getElementById('quote-box').getElementsByClassName('
 
 let setQuote = '';
 let setInfo = '';
+let quotesBySelectedTabArray = [];
 
 let quotes = [
 	{
@@ -37,7 +38,7 @@ let quotes = [
 	}
 ];
 
-let totalNumberQuotes = quotes.length;
+
 
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
@@ -57,12 +58,16 @@ function random_bg_color() {
 
 
 function getRandomQuote() {
+	quotesBySelectedTab(); 
+	console.log(quotesBySelectedTabArray);
+	let totalNumberQuotes = quotesBySelectedTabArray.length;
+	
 	let selectQuoteNum = getRandomInt(totalNumberQuotes);
 	
-	setQuote = quotes[selectQuoteNum].quote;
-	let selectQuoteSource = quotes[selectQuoteNum].source;
-	let selectQuoteCitation = quotes[selectQuoteNum].citation;
-	let selectQuoteYear = quotes[selectQuoteNum].year;
+	setQuote = quotesBySelectedTabArray[selectQuoteNum].quote;
+	let selectQuoteSource = quotesBySelectedTabArray[selectQuoteNum].source;
+	let selectQuoteCitation = quotesBySelectedTabArray[selectQuoteNum].citation;
+	let selectQuoteYear = quotesBySelectedTabArray[selectQuoteNum].year;
 
 	/* 	setInfo = selectQuoteSource + '<span class="citation">' + selectQuoteCitation + '</span><span class="year">' + selectQuoteYear + '</span>'; */
    
@@ -79,8 +84,18 @@ function printQuote() {
 	getRandomQuote();
 	document.getElementById('quote-box').getElementsByClassName('quote')[0].innerHTML = setQuote;
 	document.getElementById('quote-box').getElementsByClassName('source')[0].innerHTML = setInfo
-	document.body.style.backgroundColor =  random_bg_color();
+	document.body.style.backgroundColor = random_bg_color();
+/* 	console.log('check:');
+	var chk1= document.getElementById('book').checked; 
+	console.log(chk1); */
+	validatecb();
 }
+
+function removeTag(array, element) {
+    return array.filter(e => e !== element);
+}
+
+
 
 let autoQuote = setInterval(function () { printQuote() }, 3000);
 
@@ -98,14 +113,27 @@ var marvelHeroes = heroes.filter(function (hero) {
 var tagQuotes = quotes.filter(function (some) {
 	return some.year > 2000;
 });
+
+
+/* let filterByTags = ['speech', 'sequel','dontbelong'];
+filterByTags = removeTag(filterByTags, 'dontbelong');
+console.log('filterTag:');
+console.log(filterByTags);
+
+ */
+function quotesBySelectedTab() { 
+let resultcb = Array.from(document.querySelectorAll('input[type="checkbox"]:checked'));
+let filterByTags = resultcb.map(el => el.value);
+/* 	console.log("filtertags:");
+	console.log(filterByTags);
+ */
+let filterByTagMaps = filterByTags.reduce((map, tag) => map.set(tag, true), new Map());
+let resulttagfilter = quotes.filter((o) => o.tags.some((tag) => filterByTagMaps.get(tag)));
 	
-const filterByTags = ['speech', 'sequel'];
+	return quotesBySelectedTabArray = resulttagfilter;
 
-const filterByTagMaps = filterByTags.reduce((map, tag) => map.set(tag, true), new Map());
+};
 
-const result = quotes.filter((o) => o.tags.some((tag) => filterByTagMaps.get(tag)));
-
-console.log(result);
 
 var books = [
     {
@@ -142,6 +170,7 @@ bookCheckouts = bookCheckouts.reduce((a, b) => a.concat(b), []);
 /* FILTER OUT DUPS */
 bookCheckouts = bookCheckouts.filter(function (v, i) { return bookCheckouts.indexOf(v) == i; });
 
+
 /* FILTER OUT DUPS another version  
 let resultQ = bookCheckouts.sort().reduce((accumulator, current) => {
     const length = accumulator.length
@@ -167,5 +196,38 @@ var bookCheckouts = quotes.reduce(function(accumulator, currentValue) {
   }, ['Alphabet']);
  */
 
+console.log("booksCheckout:");
+console.log(bookCheckouts);
 
-console.log(bookCheckoutsObj)
+function validatecb() {
+	let chks = document.querySelectorAll("#tagcheckbox input[type='checkbox']");
+
+/* 	let resultcb = Array.prototype.every.call(chks, function (c) {
+		console.log("cb 0:");
+		console.log(c.checked);
+		return c.checked;
+	});
+ 
+	selected = Array.prototype.filter.apply(
+		select.options, [
+		  function(o) {
+			return o.selected;
+		  }
+		]
+	);
+	let resultcb = chks.forEach(function(element) {
+		console.log(element.name + " " + element.checked);
+		return element.name;
+
+	
+	let resultcb = Array.from(chks, function(element) {
+		console.log(element.name + " " + element.checked);
+		if (element.checked) { return element.name };
+*/
+	let resultcb = Array.from(document.querySelectorAll('input[type="checkbox"]:checked'));
+	let values = resultcb.map(el => el.value);
+
+	
+}
+
+validatecb();
