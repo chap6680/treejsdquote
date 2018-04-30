e76adc06293ff8d8aab2b4977e76c80adc77bff3
david chapman <chap6680@gmail.com>
2018-04-29 22:22:56 -0600

    preliminary final

--- js/script.js
+++ js/script.js
@@ -1,77 +1,147 @@
-/* const uiQuoteBox =document.getElementById('quote-box').getElementsByClassName('citation')[0].innerHTML; */
-const uiQuoteBox = document.getElementById('quote-box').getElementsByClassName('citation')[0];
-
-/* console.log(uiQuoteBox);
- */
-
 let setQuote = '';
 let setInfo = '';
-let quotesBySelectedTabArray = [];
+let quotes = [];
 
-let quotes = [
+let quoteSet = [
 	{
-		quote: 'firstq',
-		source: 'author2',
-		citation: 'book',
-		year: 1997,
-		tags: ['speech','book','presidental']
+		quote: 'If you could kick the person in the pants responsible for most of your trouble, you wouldn’t sit for a month.',
+		source: 'Theodore Roosevelt',
+		year: 1904,
+		citation: '',
+		tags: ['presidental']
 	},
 	{
-		quote: 'secondq',
-		source: 'author2',
-		citation: 'book2',
-		year: 2017,
-		tags: ['speech','sequel','book','presidental']
+		quote: 'It is amazing what you can accomplish if you do not care who gets the credit.',
+		source: 'Harry Truman',
+		year: 1947,
+		citation: '',
+		tags: ['presidental']
 	},
 	{
-		quote: 'Don\'t cry because it\'s over, smile because it happened.',
-		source: 'Dr. Suess',
-		year: 1998,
-		tags: ['book']
+		quote: 'Efforts and courage are not enough without purpose and direction.',
+		source: 'John Kennedy',
+		year: 1961,
+		citation: '',
+		tags: ['presidental']
+	},
+	{
+		quote: 'Associate yourself with men of good quality if you esteem your own reputation; for \'tis better to be alone than in bad company.',
+		source: 'George Washington',
+		year: 1794,
+		citation: '',
+		tags: ['presidental']
+	},
+	{
+		quote: 'If your actions inspire others to dream more, learn more, do more and become more, you are a leader',
+		source: 'John Quincy Adams',
+		year: 1827,
+		citation: '',
+		tags: ['presidental']
+	},
+	{
+		quote: 'No one can make you feel inferior without your consent. ',
+		source: 'Eleanor Roosevelt',
+		year: 1937,
+		citation: 'This is My Story',
+		tags: ['presidental', 'wisdom', 'book']
+	},
+	{
+		quote: 'The fool doth think he is wise, but the wise man knows himself to be a fool. ',
+		source: 'William Shakespeare',
+		citation: 'As You Like It',
+		year: 1623,
+		tags: ['wisdom', 'book']
+	},
+	{
+		quote: 'The only true wisdom is in knowing you know nothing. ',
+		source: 'Socrates',
+		year: 0,
+		citation: '',
+		tags: ['wisdom']
+	},
+	{
+		quote: 'Count your age by friends, not years. Count your life by smiles, not tears. ',
+		source: 'John Lennon',
+		citation:'',
+		year: 0,
+		tags: ['wisdom']
+	},
+	{
+		quote: 'A wise man will make more opportunities than he finds. ',
+		source: 'Francis Bacon',
+		year: 1597,
+		citation:'Book of Essays',
+		tags: ['wisdom', 'book']
+	},
+	{
+		quote: 'I am one with the Force, the Force is with me. ',
+		source: 'Chirrut Imwe',
+		year: 2016,
+		citation: 'Rogue One',
+		tags: ['movie']
+	},
+	{
+		quote: 'Where we are going, we don’t need roads.',
+		source: 'Dr. Emmet Brown',
+		year: 1985,
+		citation:'Back to the Future',
+		tags: ['movie']
+	},
+	{
+		quote: 'We\'re going to need a bigger boat.',
+		source: 'Martin Brody ',
+		year: 1977,
+		citation: 'Jaws',
+		tags: ['movie']
 	},
 	{
 		quote: "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.",
 		source: 'J.K. Rowling',
 		citation: 'Harry Potter and the Goblet of Fire',
 		year: 2017,
-		tags: ['book','sequel','presidental']
-	}
+		tags: ['book']
+	}	
 ];
-
 
 
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-
-function getRandomInt(max) {
-	return Math.floor(Math.random() * Math.floor(max));
-  }
-
-function random_bg_color() {
-	let x = Math.floor(Math.random() * 256);
-	let y = Math.floor(Math.random() * 256);
-	let z = Math.floor(Math.random() * 256);
-	return "rgb(" + x + "," + y + "," + z + ")";
+/* Main function called - first gets the quote from another function.
+then this function displays in the DOM - the quote and changes the background color
+*/
+function printQuote() { 
+	getRandomQuote();
+	document.getElementById('quote-box').getElementsByClassName('quote')[0].innerHTML = setQuote;
+	document.getElementById('quote-box').getElementsByClassName('source')[0].innerHTML = setInfo
+	document.body.style.backgroundColor = random_bg_color();
+/* 	validatecb();  REMOVE  */
 }
 
-
+/* Workhorse function - 
+	-First looks at the selected tabs - and filters the entire quote list down to just the tabs selected.
+	-Gets random number based on quotes selected, and sets fields to temp variables
+	-creates two variables to take to the screen.  quote and citation/source/year
+ */
 function getRandomQuote() {
 	quotesBySelectedTab(); 
-	console.log(quotesBySelectedTabArray);
-	let totalNumberQuotes = quotesBySelectedTabArray.length;
+ 	console.log(quotes); 
+	let totalNumberQuotes = quotes.length;
 	
 	let selectQuoteNum = getRandomInt(totalNumberQuotes);
 	
-	setQuote = quotesBySelectedTabArray[selectQuoteNum].quote;
-	let selectQuoteSource = quotesBySelectedTabArray[selectQuoteNum].source;
-	let selectQuoteCitation = quotesBySelectedTabArray[selectQuoteNum].citation;
-	let selectQuoteYear = quotesBySelectedTabArray[selectQuoteNum].year;
+	let selectQuoteSource = quotes[selectQuoteNum].source;
+	let selectQuoteCitation = quotes[selectQuoteNum].citation;
+	let selectQuoteYear = quotes[selectQuoteNum].year;
 
-	/* 	setInfo = selectQuoteSource + '<span class="citation">' + selectQuoteCitation + '</span><span class="year">' + selectQuoteYear + '</span>'; */
-   
+	// Main Quote
+	setQuote = quotes[selectQuoteNum].quote;
+
+	// Puts the source/author on new variable
 	setInfo = selectQuoteSource;
+
+	// then adds the citation, and year - only if they exist
 	if (!(selectQuoteCitation == '' || selectQuoteCitation === undefined || selectQuoteCitation == null || selectQuoteCitation.length <= 0)) { 
 		setInfo = setInfo + '<span class="citation">' + selectQuoteCitation + '</span>';
 	}
@@ -80,154 +150,57 @@ function getRandomQuote() {
 	}
 }	
 
-function printQuote() { 
-	getRandomQuote();
-	document.getElementById('quote-box').getElementsByClassName('quote')[0].innerHTML = setQuote;
-	document.getElementById('quote-box').getElementsByClassName('source')[0].innerHTML = setInfo
-	document.body.style.backgroundColor = random_bg_color();
-/* 	console.log('check:');
-	var chk1= document.getElementById('book').checked; 
-	console.log(chk1); */
-	validatecb();
-}
-
-function removeTag(array, element) {
-    return array.filter(e => e !== element);
-}
-
-
-
-let autoQuote = setInterval(function () { printQuote() }, 3000);
-
-var heroes = [
-	{name: 'Batman', franchise: 'DC'},
-	{name: 'Ironman', franchise: 'Marvel'},
-	{name: 'Thor', franchise: 'Marvel'},
-	{name: 'Superman', franchise: 'DC'}
-];
-
-var marvelHeroes = heroes.filter(function (hero) {
-	return hero.franchise == 'Marvel';
+/* Master Tag list - this  */
+var bookCheckouts = quoteSet.map(function (item) {
+	return item.tags;
 });
-
-var tagQuotes = quotes.filter(function (some) {
-	return some.year > 2000;
-});
-
-
-/* let filterByTags = ['speech', 'sequel','dontbelong'];
-filterByTags = removeTag(filterByTags, 'dontbelong');
-console.log('filterTag:');
-console.log(filterByTags);
-
- */
-function quotesBySelectedTab() { 
-let resultcb = Array.from(document.querySelectorAll('input[type="checkbox"]:checked'));
-let filterByTags = resultcb.map(el => el.value);
-/* 	console.log("filtertags:");
-	console.log(filterByTags);
- */
-let filterByTagMaps = filterByTags.reduce((map, tag) => map.set(tag, true), new Map());
-let resulttagfilter = quotes.filter((o) => o.tags.some((tag) => filterByTagMaps.get(tag)));
-	
-	return quotesBySelectedTabArray = resulttagfilter;
-
-};
-
-
-var books = [
-    {
-        title: "Showings",
-        author: "Julian of Norwich",
-        checkouts: 45
-    },
-    {
-        title: "The Triads",
-        author: "Gregory Palamas",
-        checkouts: 32
-    },
-    {
-        title: "The Praktikos",
-        author: "Evagrius Ponticus",
-        checkouts: 29
-    }
-];
-
-
-/* const bookCheckouts = [...new Set(quotes.map(item => item.tags))]; */
-/* let bookCheckouts = quotes.map(item => item.source).filter((value, index, self) => self.indexOf(value) === index);
- */
-var bookCheckouts = quotes.map(function(item) {
-    return item.tags;
-});
-/* bookCheckouts = bookCheckouts.filter(function (v, i) { return bookCheckouts.indexOf(v) == i; });
- */
-/* let uniqA = [...new Set(bookCheckouts)];
- */
 
 bookCheckouts = bookCheckouts.reduce((a, b) => a.concat(b), []);
 
 /* FILTER OUT DUPS */
 bookCheckouts = bookCheckouts.filter(function (v, i) { return bookCheckouts.indexOf(v) == i; });
 
-
-/* FILTER OUT DUPS another version  
-let resultQ = bookCheckouts.sort().reduce((accumulator, current) => {
-    const length = accumulator.length
-    if (length === 0 || accumulator[length - 1] !== current) {
-        accumulator.push(current);
-    }
-    return accumulator;
-}, []);
-console.log(resultQ); //[1,2,3,4,5]
- */
-
-/* let bookCheckoutsObj = bookCheckouts.reduce(function (acc, cur, i) {
-	acc[i] = cur;
-	return acc;
-}, {}); 
- */  
-let bookCheckoutsObj = Object.assign(...bookCheckouts.map(d => ({ [d]: true })));
-
-
-/* this was the most condensed but didnt give unique values - listed all
-var bookCheckouts = quotes.reduce(function(accumulator, currentValue) {
-	return [...accumulator, ...currentValue.tags];
-  }, ['Alphabet']);
- */
-
-console.log("booksCheckout:");
 console.log(bookCheckouts);
 
-function validatecb() {
-	let chks = document.querySelectorAll("#tagcheckbox input[type='checkbox']");
+/* Filtering quotes array
+This function first gets which checkboxes were selected - creates an array. 
 
-/* 	let resultcb = Array.prototype.every.call(chks, function (c) {
-		console.log("cb 0:");
-		console.log(c.checked);
-		return c.checked;
-	});
- 
-	selected = Array.prototype.filter.apply(
-		select.options, [
-		  function(o) {
-			return o.selected;
-		  }
-		]
-	);
-	let resultcb = chks.forEach(function(element) {
-		console.log(element.name + " " + element.checked);
-		return element.name;
-
-	
-	let resultcb = Array.from(chks, function(element) {
-		console.log(element.name + " " + element.checked);
-		if (element.checked) { return element.name };
+Then it takes the quote Array and filters it - keeping those quotes that have tags that match the checkboxes
 */
+function quotesBySelectedTab() { 
 	let resultcb = Array.from(document.querySelectorAll('input[type="checkbox"]:checked'));
-	let values = resultcb.map(el => el.value);
+	console.log('resultagfilter:');
+	console.log(resultcb);
+	let filterByTags = resultcb.map(el => el.value);
+	let filterByTagMaps = filterByTags.reduce((map, tag) => map.set(tag, true), new Map());
+	let resulttagfilter = quoteSet.filter((o) => o.tags.some((tag) => filterByTagMaps.get(tag)));
+	console.log(resulttagfilter);
+	return quotes = resulttagfilter;
+};
 
-	
+
+
+//Following functions are miscellaneous supporting items
+// support function to get random number 
+function getRandomInt(max) {
+	return Math.floor(Math.random() * Math.floor(max));
+  }
+
+// support function - get color for random background
+function random_bg_color() {
+	let x = Math.floor(Math.random() * 256);
+	let y = Math.floor(Math.random() * 256);
+	let z = Math.floor(Math.random() * 256);
+	return "rgb(" + x + "," + y + "," + z + ")";
 }
 
-validatecb();
+//remove if works
+/* function removeTag(array, element) {
+    return array.filter(e => e !== element);
+}
+ */
+
+//this will change the quote automatically.  Time is 3 seconds - not production
+//time but allows for testing 
+let autoQuote = setInterval(function () { printQuote() }, 3000);
+
